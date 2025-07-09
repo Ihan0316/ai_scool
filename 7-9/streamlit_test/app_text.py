750508d8b5b590c621f9578642ea8a692f9a7444
“Ihan0316” <aijnjj12@gmail.com>
2025-07-09 13:18:35 +0900

    7/9 streamlit 설정

--- 7-9/streamlit_test/app_text.py
+++ 7-9/streamlit_test/app_text.py
@@ -4,5 +4,13 @@ st.title("제목 (Title)")
 st.header("헤더 (Header)")
 st.subheader("서브헤더 (Subheader)")
 st.text("일반 텍스트 (Text)")
-st.markdown("`Markdown` 지원합니다.")
+st.markdown("`Markdown` **지원**합니다.")
 st.code("print('Hello, Streamlit!')")
+
+code1 = '''
+import streamlit as st
+st.title("제목")
+st.header("헤더")
+'''
+
+st.code(code1, language='python')
