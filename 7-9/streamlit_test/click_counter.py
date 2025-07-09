15351b530b75a750f3960f89c9fd76376aa3739f
“Ihan0316” <aijnjj12@gmail.com>
2025-07-09 16:35:03 +0900

    7/9 streamlit tutorial

--- 7-9/streamlit_test/click_counter.py
+++ 7-9/streamlit_test/click_counter.py
@@ -6,4 +6,4 @@ if 'count' not in st.session_state:
 if st.button('카운트'):
     st.session_state.count += 1
 
-st.write('버튼 클릭 횟수:', st.session_state.count)
+st.write(f'버튼 클릭 횟수:{st.session_state.count}')
